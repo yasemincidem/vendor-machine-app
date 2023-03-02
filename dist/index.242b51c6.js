a66d098b90ec0bbc1f1934671ea1e2785d34baec
yasemincidem <yasemincidem@gmail.com>
2023-03-03 00:58:38 +0300

    created vending machine app

---
+++ dist/index.242b51c6.js
@@ -0,0 +1,34 @@
+const items = [
+    {
+        name: "Coca cola",
+        number: 1,
+        count: 3,
+        price: "1.20"
+    },
+    {
+        name: "Pepsi",
+        number: 2,
+        count: 3,
+        price: "2.40"
+    },
+    {
+        name: "M & M",
+        number: 3,
+        count: 5,
+        price: "3.23"
+    },
+    {
+        name: "Chocolate",
+        number: 3,
+        count: 2,
+        price: "4.33"
+    },
+    {
+        name: "Crackers",
+        number: 4,
+        count: 5,
+        price: "6.23"
+    }
+];
+
+//# sourceMappingURL=index.242b51c6.js.map
